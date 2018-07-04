a435405873558ff6486876140194c4bab5a827c7
Linguo Niu <linguo.niu@huabaotech.com>
2018-07-04 15:31:03 +0800

    修改跳转方式

--- app/src/main/java/com/niles/caigousdkdemo/MainActivity.java
+++ app/src/main/java/com/niles/caigousdkdemo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.niles.caigousdkdemo;
 
+import android.content.ComponentName;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -15,15 +16,57 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void openCaiGou(View view) {
-        final Intent intent = new Intent(Intent.ACTION_VIEW);
-        final Uri data = Uri.parse("proucrement://user=admin,password=admin,server=192.168.1.1,port=8080");
-        intent.setData(data);
+    public void openCaiGou1(View view) {
+        Intent intent = createIntent1();
+        openCaiGou(intent);
+    }
+
+    public void openCaiGou2(View view) {
+        Intent intent = createIntent2();
+        openCaiGou(intent);
+    }
+
+    public void openCaiGou3(View view) {
+        Intent intent = createIntent3();
+        openCaiGou(intent);
+    }
+
+    private void openCaiGou(Intent intent) {
+        if (intent == null) {
+            Toast.makeText(this, "没有安装采购App(-1)", Toast.LENGTH_SHORT).show();
+            return;
+        }
         try {
             startActivity(intent);
         } catch (Exception e) {
             e.printStackTrace();
-            Toast.makeText(this, "没有安装采购App", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "没有安装采购App(-2)", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    private Intent createIntent1() {
+        final Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("proucrement://user=admin,password=admin,server=192.168.1.1,port=8080"));
+        return intent;
+    }
+
+    private Intent createIntent2() {
+        final Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("proucrement://"));
+        intent.putExtra("user", "admin");
+        intent.putExtra("password", "admin");
+        intent.putExtra("server", "192.168.1.1");
+        intent.putExtra("port", "8080");
+        return intent;
+    }
+
+    private Intent createIntent3() {
+        final Intent intent = new Intent();
+        intent.setComponent(new ComponentName("com.zhu.procurement", "com.zhu.ec.mainmenu.MainActivity"));
+        intent.putExtra("user", "admin");
+        intent.putExtra("password", "admin");
+        intent.putExtra("server", "192.168.1.1");
+        intent.putExtra("port", "8080");
+        return intent;
     }
 }
